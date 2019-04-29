7d7bd971848d5c537ea2f73bc4f1e20ff91abf06
Mary Selig <seligmar@gmail.com>
2019-04-29 18:45:55 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -26,6 +26,7 @@ def move(board, index, current_player)
   board[index] = current_player
 end
 
+
 def valid_move?(board, index)
   index.between?(0,8) && !position_taken?(board, index)
 end
@@ -132,5 +133,3 @@ end
   puts "Cat's Game!"
 end 
 end
-
-
